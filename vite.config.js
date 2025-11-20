ed79e313df8fbd5c8f486eff144941c19124c365
kasheesh711 <kevhsh7@gmail.com>
2025-11-20 20:17:43 +0700

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increases warning threshold to 1000kb
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split external libraries into separate chunks
+          'vendor-react': ['react', 'react-dom'],
+          'vendor-recharts': ['recharts'],
+          'vendor-icons': ['lucide-react'],
+          'vendor-utils': ['lodash'] // if you use lodash
+        }
+      }
+    }
+  }
 })
